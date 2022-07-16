7b8430e61f987c12e2e4346dd92c930f6b93bdf2
Brock Hayes <brockhayes123@gmail.com>
2022-07-16 14:09:38 -0500

    Added the file structure for procs and views along with the migrations needed to force them to run everytime the database is updated.

---
+++ ExampleService.Data/Migrations/99999999999996_ExampleService_Procs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExampleService.Data.Migrations
+{
+    public partial class ExampleService_Procs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ExampleService.Data/Migrations/99999999999999_RemoveViewsProcsandTriggerMigrationHistory2.Designer.cs
+++ ExampleService.Data/Migrations/99999999999999_RemoveViewsProcsandTriggerMigrationHistory2.Designer.cs
@@ -3,6 +3,7 @@ using ExampleService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExampleService.Data.Migrations
 {
     [DbContext(typeof(ExampleServiceDataContext))]
-    partial class ExampleServiceDataContextModelSnapshot : ModelSnapshot
+    [Migration("99999999999999_RemoveViewsProcsandTriggerMigrationHistory2")]
+    partial class RemoveViewsProcsandTriggerMigrationHistory2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
